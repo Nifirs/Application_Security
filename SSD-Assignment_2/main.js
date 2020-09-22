831976824b43ec27a1e95c684dec864d09b6fa05
Nilfa Sahilan <42144778+Nifirs@users.noreply.github.com>
2020-09-23 00:07:12 +0530

    Add files via upload

---
+++ SSD-Assignment_2/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+     
+    // client id of the project
+
+    var clientId = "298623744882-fhdsmpjtrm5r9uek5g07854buvpdvb3t.apps.googleusercontent.com";
+
+    // redirect_uri of the project
+
+    var redirect_uri = "http://localhost/SSD-Assignment_2/upload.html";
+
+    // scope of the project
+
+    var scope = "https://www.googleapis.com/auth/drive";
+
+    // the url to which the user is redirected to
+
+    var url = "";
+
+
+    // this is event click listener for the button
+
+    $("#login").click(function(){
+
+       // this is the method which will be invoked it takes four parameters
+
+       signIn(clientId,redirect_uri,scope,url);
+
+    });
+
+    function signIn(clientId,redirect_uri,scope,url){
+     
+       // the actual url to which the user is redirected to 
+
+       url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+       +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+       +"&access_type=offline";
+
+       // this line makes the user redirected to the url
+
+       window.location = url;
+
+    }
+});
